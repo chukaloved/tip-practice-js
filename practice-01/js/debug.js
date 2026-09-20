64a9634f58fe25bf055c2ba359f08a23e88d12ca
chukaloved <chukalov.e@yandex.ru>
2026-09-20 13:15:01 +0300

    Сделано задание 5, изменено содержание readme.md, добавлены скриншоты выводов и содержания консоли для задания 5

--- practice-01/js/debug.js
+++ practice-01/js/debug.js
@@ -4,15 +4,15 @@ const plannedText = "8";
 const completedText = "3";
 const additionalText = "2";
 
-const completedTotal = completedText + additionalText;
-const remainingTasks = plannedText - completedTotal;
+const completedTotal = Number(completedText) + Number(additionalText);
+const remainingTasks = Number(plannedText) - completedTotal;
 
 console.log("Выполнено:", completedTotal);
 console.log("Осталось:", remainingTasks);
 
 let controlSum = 0;
 
-for (let taskNumber = 1; taskNumber < 4; taskNumber += 1) {
+for (let taskNumber = 1; taskNumber <= 4; taskNumber += 1) {
   controlSum += taskNumber;
 }
 
